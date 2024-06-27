eed1b3b89eeb39b8cf00fd3f32022a6d581959c6
Ankit Chouhan <shankit119@gmail.com>
2024-06-27 10:05:02 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.get("/", (req,res) =>{
     res.json("hello");
 })
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`server is running on port ${port}`))
 
